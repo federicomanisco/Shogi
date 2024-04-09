99de4a6f25e1e8ed9762708d214ce46aec349516
hyperion-06 <132679390+hyperion-06@users.noreply.github.com>
2024-04-09 22:29:15 +0200

    "merge commit"
    Rota -> Main

--- Shogi/Shogi/Form1.cs
+++ Shogi/Shogi/Form1.cs
@@ -1,4 +1,6 @@
 using System.CodeDom;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing.Text;
 
 namespace Shogi
 {
@@ -19,6 +21,13 @@ namespace Shogi
         Kubomawashi kubomawashi_sfidante = new Kubomawashi(); //lo sfidante inizia sotto
         Kubomawashi kubomawashi_sfidato = new Kubomawashi();  //lo sfidato inizia sopra
         int kubomawashi_width = 300; //lunghezza lato kubomawashi, quadrato
+
+        protected string PERCORSOIMMAGINE = Application.StartupPath;
+        int timer_width = 400;
+        int timer_height = 240;
+        int tempoMin = 10; //tempo di gioco per giocatore, minuti
+        int tempoSec = 30; //tempo di gioco per giocatore, secondi
+        bool turno = true;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -54,6 +63,8 @@ namespace Shogi
             scriviNumeriCaselle();
             generaPosizioneIniziale();
             disegnaKubomawashi(kubomawashi_width);
+            disegnaTimer((timer_width, timer_height), tempoMin, tempoSec);
+            this.BackgroundImage = Image.FromFile($"{PERCORSOIMMAGINE}/shogiPieces/extra/woodenTable.jpg");
         }
 
         public (int, int) getRowColFromLocation(Point point)
@@ -98,6 +109,7 @@ namespace Shogi
                 lbl.Size = new Size(30, 30);
                 lbl.Font = new Font("Arial", 18);
                 lbl.AutoSize = true;
+                lbl.BackColor = Color.Transparent;
                 Controls.Add(lbl);
             }
 
@@ -109,6 +121,7 @@ namespace Shogi
                 lbl.Size = new Size(30, 30);
                 lbl.Font = new Font("Arial", 18);
                 lbl.AutoSize = true;
+                lbl.BackColor = Color.Transparent;
                 Controls.Add(lbl);
             }
         }
@@ -202,26 +215,66 @@ namespace Shogi
             mostraCasella(torreBianca);
         }
 
+        private void disegnaTimer((int, int) grandezza, int min, int sec)
+        {
+            int width = grandezza.Item1;
+            int height = grandezza.Item2;
+
+            pbox_timer1.Size = new Size(width, height);
+            pbox_timer1.Location = new Point(60, 1080 - height - (1080 - (GRIDSIZE * TILESIZE)) / 2);
+            pbox_timer1.BackColor = Color.Transparent;
+            pbox_timer1.Image = Image.FromFile($"{PERCORSOIMMAGINE}/shogiPieces/extra/timerReal.png");
+            pbox_timer1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pbox_timer2.Size = new Size(width, height);
+            pbox_timer2.Location = new Point(1920 - width - 60, height - 160);
+            pbox_timer2.BackColor = Color.Transparent;
+            pbox_timer2.Image = Image.FromFile($"{PERCORSOIMMAGINE}/shogiPieces/extra/timerReal.png");
+            pbox_timer2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile($@"{PERCORSOIMMAGINE}/shogiPieces/extra/numberFont.ttf");
+
+            lbl_Min1.Text = min.ToString();
+            lbl_Min1.Font = new Font(pfc.Families[0], 80);
+            lbl_Min1.ForeColor = Color.FromArgb(255, 38, 42);
+            lbl_Min1.BackColor = Color.FromArgb(40, 40, 40);
+
+            lbl_Sec1.Text = sec.ToString();
+            lbl_Sec1.Font = new Font(pfc.Families[0], 80);
+            lbl_Sec1.ForeColor = Color.FromArgb(255, 38, 42);
+            lbl_Sec1.BackColor = Color.FromArgb(40, 40, 40);
+            lbl_Sec1.Location = new Point(60 + 225, 1080 - height - (1080 - (GRIDSIZE * TILESIZE)) + 227); // x +215 e y +233 per centrare il testo
+
+            lbl_Min2.Text = min.ToString();
+            lbl_Min2.Font = new Font(pfc.Families[0], 80);
+            lbl_Min2.ForeColor = Color.FromArgb(255, 38, 42);
+            lbl_Min2.BackColor = Color.FromArgb(40, 40, 40);
+
+
+            lbl_Sec2.Text = sec.ToString();
+            lbl_Sec2.Font = new Font(pfc.Families[0], 80);
+            lbl_Sec2.ForeColor = Color.FromArgb(255, 38, 42);
+            lbl_Sec2.BackColor = Color.FromArgb(40, 40, 40);
+            lbl_Sec2.Location = new Point(1920 - width - 60 + 223, height - 160 + 62); // x +42 e y +233 per centrare il testo
+
+            lbl_Min1.Location = new Point(60 + 62, 1080 - height - (1080 - (GRIDSIZE * TILESIZE)) + 227); // x +62 e y +233 per centrare il testo
+            lbl_Min2.Location = new Point(1920 - width - 60 + 60, height - 160 + 62); // x +60 e y +67 per centrare il testo
+
+        }
+
         private void disegnaKubomawashi(int width)
         {
-            kubomawashi1.Location = new Point(100,162 - 40);
+            kubomawashi1.Location = new Point(100, 162 - 40);
             kubomawashi1.BackColor = TileColor;
             kubomawashi1.Size = new Size(width, width);
             kubomawashi1.BorderStyle = BorderStyle.FixedSingle;
-            kubomawashi2.Location = new Point(1920 - 100 - width, 162 - 40 + TILESIZE*GRIDSIZE - width);
+            kubomawashi2.Location = new Point(1920 - 100 - width, 162 - 40 + TILESIZE * GRIDSIZE - width);
             kubomawashi2.BackColor = TileColor;
             kubomawashi2.Size = new Size(width, width);
             kubomawashi2.BorderStyle = BorderStyle.FixedSingle;
         }
 
-        /// ////////////////////////////////////////////////////
-        private void disegnaCronometri()
-        {
-
-
-
-        }        
-        /// ////////////////////////////////////////////////////
 
         private void Tile_Click(object sender, EventArgs e)
         {
@@ -305,5 +358,64 @@ namespace Shogi
             return mosseRegolari;
         }
 
+        private void timer_tick(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                int min;
+                int sec;
+                string player;
+
+                if (turno) player = "SFIDANTE";
+                else player = "SFIDATO";
+
+                if (turno)
+                {
+                    min = int.Parse(lbl_Min1.Text);
+                    sec = int.Parse(lbl_Sec1.Text);
+                }
+                else
+                {
+                    min = int.Parse(lbl_Min2.Text);
+                    sec = int.Parse(lbl_Sec2.Text);
+                }
+
+                if (sec == 1)
+                {
+                    if (min == 0)
+                    {
+                        if (turno) lbl_Sec1.Text = "0";
+                        else lbl_Sec2.Text = "0";
+                        timer1.Stop();
+                        MessageBox.Show($"LO {player} PERDE PER TEMPO");
+                        sec = 0;
+                    }
+                    else
+                    {
+                        sec = 60;
+                        min--;
+                    }
+                }
+                else sec--;
+
+
+                if (turno)
+                {
+                    lbl_Min1.Text = min.ToString();
+                    lbl_Sec1.Text = sec.ToString();
+                }
+                else
+                {
+                    lbl_Min2.Text = min.ToString();
+                    lbl_Sec2.Text = sec.ToString();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            turno = !turno;
+        }
     }
 }
